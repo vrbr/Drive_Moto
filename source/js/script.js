7c2d94a901a149179a4b8c466b410dd14e834e4b
Мария <lpvamaria@gmail.com>
2021-08-18 23:32:37 +0300

    Реализован блок с поиском товаров

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,21 @@
 $(function(){
+
+    //слайдер
+
     $('.banner-section__slider').slick({
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-prev"><img src="img/arrow-left.svg" alt=""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-next"><img src="img/arrow-right.svg" alt=""></button>'
     });
+
+    //поиск
+
+    $('.search__tabs-item').on('click', function(e) {
+        e.preventDefault();
+        $('.search__tabs-item').removeClass('search__tabs-item--active');
+        $('.search__content-item').removeClass('search__content-item--active');
+
+        $(this).addClass('search__tabs-item--active');
+        $($(this).attr('href')).addClass('search__content-item--active');
+    })
 });
