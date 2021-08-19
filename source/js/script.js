eb3e6fad1122e17d4ab61b6e2736e1d8483b4e39
Мария <lpvamaria@gmail.com>
2021-08-19 14:21:36 +0300

    Реализованы блоки с популярными товарами и категориями

--- source/js/script.js
+++ source/js/script.js
@@ -10,12 +10,26 @@ $(function(){
 
     //поиск
 
-    $('.search__tabs-item').on('click', function(e) {
+    $('.tab').on('click', function (e) {
         e.preventDefault();
-        $('.search__tabs-item').removeClass('search__tabs-item--active');
-        $('.search__content-item').removeClass('search__content-item--active');
+    
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+    
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    
+        $('.product-slider').slick('setPosition');
+    });
 
-        $(this).addClass('search__tabs-item--active');
-        $($(this).attr('href')).addClass('search__content-item--active');
-    })
+    $('.product-item__favorite').on('click', function () {
+        $('.product-item__favorite').toggleClass('product-item__favorite--active')
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btn-prev"><img src="img/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btn-next"><img src="img/arrow-black-right.svg" alt=""></button>'
+    });
 });
