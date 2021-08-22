3a643d5e2f4b3083e07ee5bfd17f424ca7a33735
Мария <lpvamaria@gmail.com>
2021-08-22 15:59:31 +0300

    Оформлены кнопки сброса и поиска, и кнопки показать по сетке и поленейно

--- source/js/script.js
+++ source/js/script.js
@@ -35,7 +35,7 @@ $(function(){
 
     $('.filter-style').styler(); //filter
 
-    $('.filter__item-drop').on('click', function() {
+    $('.filter__item-drop, .filter__extra').on('click', function() {
         $(this).toggleClass('filter__item-drop--active');
         $(this).next().slideToggle('100');
     });
@@ -45,5 +45,16 @@ $(function(){
         min: 100000,
         max: 500000,
     });
-    
+
+    $('.catalog__filter-btngrid').on('click', function() {
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+
+    $('.catalog__filter-btnline').on('click', function() {
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
+    });
 });
